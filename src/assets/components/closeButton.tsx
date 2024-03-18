f540618382585b690e213a2d3ed280fe78192fcd
Eravity <cebotaricristian92@gmail.com>
2024-03-18 23:46:21 +0200

    v2.0.0

--- src/assets/components/closeButton.tsx
+++ src/assets/components/closeButton.tsx
@@ -1,8 +1,18 @@
 import React from "react";
-import close from "../../assets/images/close.png";
 
-const CloseButton: React.FC = () => {
-  return <img className="cursor-pointer" src={close} alt="close_icon" />;
+interface CloseProps {
+  onClick: React.MouseEventHandler<HTMLButtonElement>;
+}
+
+const CloseButton: React.FC<CloseProps> = ({ onClick }) => {
+  return (
+    <h1
+      className="text-neutral-700 hover:text-neutral-600 cursor-pointer w-5 h-5 flex justify-center items-center text-4xl font-extrabold"
+      onClick={onClick}
+    >
+      &times;
+    </h1>
+  );
 };
 
 export default CloseButton;
--- src/assets/components/interface.tsx
+++ src/assets/components/interface.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+
 import NavButton from "../components/navButton";
 import Step from "../components/step";
 import StepIndicator from "./stepIndicator";
@@ -6,6 +7,7 @@ import CloseButton from "./closeButton";
 
 const Interface: React.FC = () => {
   let [step, setStep] = useState<number>(1);
+  let [open, setIsOpen] = useState<boolean>(true);
 
   function handlePrevious(): void {
     if (step > 1) setStep(step - 1);
@@ -23,26 +25,30 @@ const Interface: React.FC = () => {
 
   return (
     <>
-      <div className="w-2/4 bg-neutral-800 rounded-xl px-10 pb-10 ">
-        <div className="flex justify-end my-5 mx-[-20px]">
-          <CloseButton />
-        </div>
-        {/* prettier-ignore */}
-        <header className="container flex justify-between px-16">
+      <div className="container w-2/4 my-4 flex justify-end items-center">
+        <CloseButton onClick={() => setIsOpen(!open)} />
+      </div>
+
+      {open && (
+        <div className="w-2/4 bg-neutral-800 rounded-xl px-10 pb-10 ">
+          <div className="flex justify-end my-5 mx-[-20px]"></div>
+          {/* prettier-ignore */}
+          <header className="container flex justify-between px-16">
           <StepIndicator stepNumber={1} condition={`${step === 1 && "bg-violet-700 animate-bounce" }`} />
           <StepIndicator stepNumber={2} condition={`${step === 2 && "bg-violet-700 animate-bounce" }`}/>
           <StepIndicator stepNumber={3} condition={`${step === 3 && "bg-violet-700 animate-bounce" }`}/>
         </header>
 
-        <main className="container grid place-items-center my-16">
-          <Step step={messages[step - 1]} />
-        </main>
+          <main className="container grid place-items-center my-16">
+            <Step step={messages[step - 1]} />
+          </main>
 
-        <footer className="container flex justify-between px-16">
-          <NavButton text="Previous Step" onClick={handlePrevious} />
-          <NavButton text="Next Step" onClick={handleNext} />
-        </footer>
-      </div>
+          <footer className="container flex justify-between px-16">
+            <NavButton text="Previous Step" onClick={() => handlePrevious()} />
+            <NavButton text="Next Step" onClick={() => handleNext()} />
+          </footer>
+        </div>
+      )}
     </>
   );
 };
